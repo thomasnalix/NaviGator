d3484375fac330d62934a90be2b0d360c33cd394
Xamez <34zemax34@gmail.com>
2023-03-29 20:12:30 +0200

    Cleanup priority queue

--- src/Lib/PriorityQueue.php
+++ src/Lib/PriorityQueue.php
@@ -7,8 +7,6 @@ use SplPriorityQueue;
 class PriorityQueue extends SplPriorityQueue
 {
 
-    private array $visited = [];
-
     public function compare(mixed $priority1, mixed $priority2) : int
     {
         if ($priority1 === $priority2) {
@@ -17,22 +15,14 @@ class PriorityQueue extends SplPriorityQueue
         return $priority1 < $priority2 ? 1 : -1;
     }
 
-    public function contains($value) : bool
-    {
-        return isset($this->visited[$value]);
-    }
-
     public function insert(mixed $value, mixed $priority)
     {
         parent::insert($value, $priority);
-        $this->visited[$value] = true;
     }
 
     public function extract() : mixed
     {
-        $value = parent::extract();
-        unset($this->visited[$value]);
-        return $value;
+        return parent::extract();
     }
 
 }
