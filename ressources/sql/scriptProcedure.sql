8db29bebf34f2c7358e329684343b6c21e45ef0d
TheKingL <josukejoestar84@gmail.com>
2023-04-04 20:25:57 +0200

    change scripts

--- ressources/sql/scriptProcedure.sql
+++ ressources/sql/scriptProcedure.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE PROCEDURE CREER_UTILISATEUR(
-p_login utilisateurs.login%TYPE,
-p_nom utilisateurs.nom%TYPE,
-p_prenom utilisateurs.prenom%TYPE,
-p_mdp utilisateurs.motDePasse%TYPE,
-p_email utilisateurs.email%TYPE,
-p_img utilisateurs.imageProfil%TYPE) LANGUAGE plpgsql AS $$
+p_login nalixt.utilisateurs.login%TYPE,
+p_nom nalixt.utilisateurs.nom%TYPE,
+p_prenom nalixt.utilisateurs.prenom%TYPE,
+p_mdp nalixt.utilisateurs.motDePasse%TYPE,
+p_email nalixt.utilisateurs.email%TYPE,
+p_img nalixt.utilisateurs.imageProfil%TYPE) LANGUAGE plpgsql AS $$
 BEGIN
     INSERT INTO nalixt.utilisateurs(login, nom, prenom, motDePasse, email, imageProfil) VALUES(p_login, p_nom, p_prenom, p_mdp, p_email, p_img);
     INSERT INTO nalixt.historique(login) VALUES(p_login);
@@ -12,7 +12,7 @@ BEGIN
 END; $$;
 
 
-CREATE OR REPLACE PROCEDURE SUPPRIMER_UTILISATEUR(p_login utilisateurs.login%TYPE) LANGUAGE plpgsql AS $$
+CREATE OR REPLACE PROCEDURE SUPPRIMER_UTILISATEUR(p_login nalixt.utilisateurs.login%TYPE) LANGUAGE plpgsql AS $$
 BEGIN
     DELETE FROM nalixt.favoris WHERE login = p_login;
     DELETE FROM nalixt.historique WHERE login = p_login;
@@ -21,12 +21,12 @@ END; $$;
 
 
 CREATE OR REPLACE PROCEDURE MODIFIER_UTILISATEUR(
-p_login utilisateurs.login%TYPE,
-p_nom utilisateurs.nom%TYPE,
-p_prenom utilisateurs.prenom%TYPE,
-p_mdp utilisateurs.motDePasse%TYPE,
-p_email utilisateurs.email%TYPE,
-p_img utilisateurs.imageProfil%TYPE) LANGUAGE plpgsql AS $$
+p_login nalixt.utilisateurs.login%TYPE,
+p_nom nalixt.utilisateurs.nom%TYPE,
+p_prenom nalixt.utilisateurs.prenom%TYPE,
+p_mdp nalixt.utilisateurs.motDePasse%TYPE,
+p_email nalixt.utilisateurs.email%TYPE,
+p_img nalixt.utilisateurs.imageProfil%TYPE) LANGUAGE plpgsql AS $$
 BEGIN
     UPDATE nalixt.utilisateurs
     SET nom = p_nom, prenom = p_prenom, motDePasse = p_mdp, email = p_email, imageProfil = p_img
@@ -34,18 +34,20 @@ BEGIN
 END; $$;
 
 
-CREATE OR REPLACE PROCEDURE AJOUTER_HISTORIQUE(p_login nalixt.utilisateurs.login%TYPE, p_historique INTEGER) LANGUAGE plpgsql AS $$
+CREATE OR REPLACE PROCEDURE AJOUTER_HISTORIQUE(p_login nalixt.utilisateurs.login%TYPE, p_trajet nalixt.trajets.trajets%TYPE, p_json nalixt.trajets.json%TYPE) LANGUAGE plpgsql AS $$
 DECLARE
-    isItnull integer;
+    isTrajetExisting integer;
+    id integer;
 BEGIN
-    SELECT COUNT(*) INTO isItnull FROM nalixt.historique WHERE login = p_login;
-    IF isItnull = 0 THEN
-        UPDATE nalixt.historique SET historique = ARRAY[p_historique]
-        WHERE login = p_login;
-    ELSE
-        UPDATE nalixt.historique SET historique = array_append(historique, p_historique)
-        WHERE login = p_login;
+    SELECT COUNT(*) INTO isTrajetExisting FROM nalixt.trajets WHERE trajets = p_trajet;
+
+    IF isTrajetExisting = 0 THEN
+        call ajouter_trajet(p_trajet, p_json);
     END IF;
+
+    SELECT idTrajet INTO id FROM nalixt.trajets WHERE trajets = p_trajet;
+    UPDATE nalixt.historique SET historique = array_append(historique, id)
+    WHERE login = p_login;
 END; $$;
 
 
@@ -75,11 +77,11 @@ BEGIN
 END; $$;
 
 
-CREATE OR REPLACE PROCEDURE AJOUTER_TRAJET(p_trajets varchar array, p_json jsonb) LANGUAGE plpgsql AS $$
+CREATE OR REPLACE PROCEDURE AJOUTER_TRAJET(p_trajets nalixt.trajets.trajets%TYPE, p_json nalixt.trajets.json%TYPE) LANGUAGE plpgsql AS $$
 DECLARE
     isAlreadyExisting integer;
 BEGIN
-    SELECT COUNT(*) INTO isAlreadyExisting FROM nalixt.trajets WHERE trajets @> p_trajets;
+    SELECT COUNT(*) INTO isAlreadyExisting FROM nalixt.trajets WHERE trajets = p_trajets;
     IF isAlreadyExisting = 0 THEN
         INSERT INTO nalixt.trajets(trajets, json) VALUES(p_trajets, p_json);
     END IF;
